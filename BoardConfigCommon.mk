a8d838c28b942c7acf83d1131132178038c3ffa2
Tiktodz <kuplemarkeple@gmail.com>
2021-11-22 16:45:05 +0000

    sdm660-common: Add prebuilts `RetroMusic`
    Source: https://github.com/RetroMusicPlayer/RetroMusicPlayer
    
    Change-Id: I5e80a65bc798ac22bc88fdc4ec3f58f45b307319
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,9 @@ TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
+# Bypass build time check on uses_libs until vendor fixes all their apps
+RELAX_USES_LIBRARY_CHECK := true
+
 # Camera
 TARGET_USES_QTI_CAMERA_DEVICE := true
 
